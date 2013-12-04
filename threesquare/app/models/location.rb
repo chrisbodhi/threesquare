6f02f9a38c34b3c67e7e4f233af492c75ba016ab
chrisbodhi <cboette@gmail.com>
2013-12-04 15:47:27 -0600

    Add method to clip reviews - in the model file.

--- threesquare/app/models/location.rb
+++ threesquare/app/models/location.rb
@@ -3,8 +3,8 @@ class Location < ActiveRecord::Base
   # Error - not truncating review but
   # does add the URL
     def snippet
-      self.review.truncate(10)
-      self.review << " ...http://tinyurl.com/rv#{rand(10..99)}"
+      self.review.truncate(30)
+      #self.review << " ...http://tinyurl.com/rv#{rand(10..99)}"
     end
 
 end
