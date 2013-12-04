afeeb65e2257c3ff9c7db31fd0a09268b3df5578
chrisbodhi <cboette@gmail.com>
2013-12-04 13:59:59 -0600

    Initial commit of Rails application and updated Gemfile.

---
+++ threesquare/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threesquare::Application.config.secret_key_base = 'd2cef20b616377b8c78b3457a04484bd9e0705ceddaeed6fb3a3b7b3d6e5f9799295fe9ca7270057cc9efeaeca1433cd873a3afea904dbc5b910a51553c0e309'
