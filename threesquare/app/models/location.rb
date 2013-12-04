a2631165ee14ef4f8b28dfd2bd524e67c818be3c
chrisbodhi <cboette@gmail.com>
2013-12-04 16:02:20 -0600

    Add ability and link to delete files. Remove feature to add randomly generated URL to review.

--- threesquare/app/models/location.rb
+++ threesquare/app/models/location.rb
@@ -1,10 +1,7 @@
 class Location < ActiveRecord::Base
 
-  # Error - not truncating review but
-  # does add the URL
     def snippet
       self.review.truncate(30)
-      #self.review << " ...http://tinyurl.com/rv#{rand(10..99)}"
     end
 
 end
