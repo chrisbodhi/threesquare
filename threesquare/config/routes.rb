1ce9737b592be30b2bf92e59fe7320402aca7bf0
chrisbodhi <cboette@gmail.com>
2013-12-07 11:49:10 -0600

    Test capability for sending text messafes.

--- threesquare/config/routes.rb
+++ threesquare/config/routes.rb
@@ -2,5 +2,8 @@ Threesquare::Application.routes.draw do
 
   root 'locations#index'
 
+  get 'send_text' => 'sendtexts#index'
+  post 'send_text' => 'sendtexts#send_text_message'
+
   resources :locations
 end
