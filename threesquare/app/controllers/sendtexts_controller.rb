a9d4f5e5c843bbfbd0cfbc05e1f4988dddd4d585
chrisbodhi <cboette@gmail.com>
2013-12-07 17:55:47 -0600

    Add messaging capability. Add start of scheduling framework.

--- threesquare/app/controllers/sendtexts_controller.rb
+++ threesquare/app/controllers/sendtexts_controller.rb
@@ -2,23 +2,32 @@ class SendtextsController < ApplicationController
   def index
   end
  
+  # Creates a message string to pass to the body of the text message, taken from an array of possible messages
+  def pick_message 
+    message = ["a", "b", "c"].sample
+  end
+
   # Send a text message to a number entered via a form's POST method. Message in :body is sent from the phone number noted at :from.
 
   # sid and token and twilio stuff to be stored in separate file
+
+  # From https://www.twilio.com/blog/2012/02/adding-twilio-sms-messaging-to-your-rails-app.html
   def send_text_message
     number_to_send_to = params[:number_to_send_to]
  
-    twilio_sid = "AC711285c8dae436df2634d388afe47841"
-    # twilio_sid = 'ACef0d534bb53901be6964325064522086'
-    twilio_token = "8306f1991e164f1d69872eba04c305f2"
+    twilio_sid = 'ACef0d534bb53901be6964325064522086'
+    twilio_token = '358346907479ae440807d8af52658c31'
     twilio_phone_number = "4124074446"
  
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
  
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
-      :body => "This is an message. It gets sent to #{number_to_send_to}"
+      :to => number_to_send_to["number_to_send_to"].to_i,
+      :body => pick_message
     )
+
+    redirect_to root_path
   end
+
 end
